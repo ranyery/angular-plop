5f7d48284f1d2b9da980f4781af931bcde89b64e
ranyery <ranyerysc@gmail.com>
2023-03-02 19:22:27 -0300

    update

--- files/file.ts
+++ files/file.ts
@@ -1,63 +1,32 @@
-this.bindCurrencyControlChanges('paymentCurrency', 'receiveCurrency', 'receive.value');
-this.bindCurrencyControlChanges('receiveCurrency', 'paymentCurrency', 'payment.value');
+this.bindCurrencyControlChanges('paymentCurrency');
+this.bindCurrencyControlChanges('receiveCurrency');
 
-private bindCurrencyControlChanges(controlName: string, otherControlName: string, offerProperty: string): void {
-  this.form.get(controlName)?.valueChanges.pipe(
-    filter(value => !!value),
-    debounceTime(300),
-    distinctUntilChanged(),
-    switchMap(() =>
-      this._getUpdatedTokenPrice().pipe(
-        tap(offer =>
-          this.form.patchValue(
-            { [otherControlName]: offer[offerProperty] },
-            { emitEvent: false }
-          )
-        ),
-        catchError(error => of(null))
+bindCurrencyControlChanges(controlName: string) {
+  const control = this.form.get(controlName);
+  if (control) {
+    control.valueChanges.pipe(
+      filter((value) => !!value),
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap(() =>
+        this._getUpdatedTokenPrice().pipe(
+          tap((offer) => {
+            const targetControlName =
+              controlName === 'paymentCurrency'
+                ? 'receiveCurrency'
+                : 'paymentCurrency';
+            const targetControl = this.form.get(targetControlName);
+            if (targetControl) {
+              targetControl.patchValue(offer.receive.value, {
+                emitEvent: false
+              });
+            }
+          }),
+          catchError((error) => {
+            return of(null);
+          })
+        )
       )
-    )
-  ).subscribe();
+    ).subscribe();
+  }
 }
-
-
-
-this.form
-  .get('paymentCurrency')!
-  .valueChanges.pipe(
-    filter((value) => !!value),
-    debounceTime(300),
-    distinctUntilChanged(),
-    switchMap(() =>
-      this._getUpdatedTokenPrice().pipe(
-        tap((offer) =>
-          this.form.patchValue(
-            { receiveCurrency: offer.receive.value },
-            { emitEvent: false }
-          ),
-        ),
-        catchError(error => {
-          return of(null);
-        })
-      )
-  )
-
-this.form
-.get('receiveCurrency')!
-.valueChanges.pipe(
-  filter((value) => !!value),
-  debounceTime(300),
-  distinctUntilChanged(),
-  switchMap(() =>
-    this._getUpdatedTokenPrice().pipe(
-      tap((offer) =>
-        this.form.patchValue(
-          { paymentCurrency: offer.receive.value },
-          { emitEvent: false }
-        ),
-      ),
-      catchError(error => {
-        return of(null);
-      })
-    )
-)
